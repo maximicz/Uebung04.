361a158f12cb0a00efaea0e28524ff9425243f7e
Max <1415407@stud.hs-mannheim.de>
2014-12-04 18:41:51 +0100

    CrypterSubstitution hinzugefügt

--- TPE_UIB01_uebung04/src/hs/mannheim_TPE_UIB01_Uebung04/CrypterCaesar.java
+++ TPE_UIB01_uebung04/src/hs/mannheim_TPE_UIB01_Uebung04/CrypterCaesar.java
@@ -1,86 +1,88 @@
 package hs.mannheim_TPE_UIB01_Uebung04;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class CrypterCaesar implements Crypter {
 
-	private String message;  //Braucht man den String überhaupt? Wie wird aufgerufen?
-	private int key;
-	private List<String> list;
-	
-	public CrypterCaesar(String message, int key) {
-		this.message = message;
+	private String key;
+
+	public CrypterCaesar(String key) {
+		
 		this.key = key;
 	}
-	
-	
+
 	@Override
 	public String encrypt(String message) throws CrypterException {
-
-			   message = message.toUpperCase();                                    // UPPERCASE
-			   char[] k = message.toCharArray();                             // Umwandeln in char-Array
-
-			   for (int i=0; i<message.length(); i++) {
-			     char c = message.charAt(i);                                 // Jedes einzelnes char-Element wird angewählt
-
-			     if(((int)(c) >= 65) && ((int)(c) <= (90-key))){         // "Normale" Erhöhung des ASCII-Wertes um a
-			       c = (char)(c + key);
-			       k[i] = c;
-			     }
-			     else if (((int)(c) >(90-key)) && ((int)(c) <=90)){      // Sonderregel für das Überschreiten der Grenze
-			       c = (char)(c + key -26);
-			       k[i] = c;
-			     }
-			   }
-			   message = new String(k);
-			   return message;
 		
+		char buchstabe = key.charAt(0);
+		int key = (int) buchstabe - 64;
+		
+		message = message.toUpperCase(); 
+		char[] k = message.toCharArray();
+
+		for (int i = 0; i < message.length(); i++) {
+			char c = message.charAt(i); 
+			if (((int) (c) >= 65) && ((int) (c) <= (90 - key))) { 
+				c = (char) (c + key);
+				k[i] = c;
+			} else if (((int) (c) > (90 - key)) && ((int) (c) <= 90)) { 
+				c = (char) (c + key - 26);
+				k[i] = c;
+			}
+		}
+		message = new String(k);
+		return message;
+
 	}
 
-	
 	@Override
 	public List<String> encrypt(List<String> messages) throws CrypterException {
-		List<String> messages2 = new ArrayList<String>();
-		
-		do {
-			String a = encrypt(messages.iterator().next());
-			messages2.add(a);
+		List<String> ergebnis = new LinkedList<String>();
+		for (String message : messages) {
+			ergebnis.add(encrypt(message));
 		}
-		while(messages.iterator().hasNext()); 
-			
-		return messages2;
+		return ergebnis;
 	}
 
 	@Override
 	public String decrypt(String cypherText) throws CrypterException {
 
-			   cypherText = cypherText.toUpperCase();                                    // UPPERCASE
-			   char[] k = cypherText.toCharArray();                             // Umwandeln in char-Array
+		
+		char buchstabe = key.charAt(0);
+		int key = (int) buchstabe + 64 *(-1);
+		
+		cypherText = cypherText.toUpperCase(); 
+		char[] k = cypherText.toCharArray(); 
 
-			   for (int i=0; i<cypherText.length(); i++) {
-			     char c = cypherText.charAt(i);                                 // Jedes einzelnes char-Element wird angewählt
+		for (int i = 0; i < cypherText.length(); i++) {
+			char c = cypherText.charAt(i); 
+			if (((int) (c) >= (65 + key)) && ((int) (c) <= (90))) { 
+				c = (char) (c - key);
+				k[i] = c;
+			} 
+			else if (((int) (c) >= 65) && ((int) (c) < (65 + key))) { 
 
-			     if(((int)(c) >= (65+key)) && ((int)(c) <= (90))){         // "Normale" Erniedrigung des ASCII-Wertes um a
-			       c = (char)(c - key);
-			       k[i] = c;
-			     }
-			     else if (((int)(c) >= 65) && ((int)(c) <(65+key))){      // Sonderregel für das Überschreiten der Grenze
-			       
-			   c = (char)(c - key +26);
+				c = (char) (c - key + 26);
 
-			       k[i] = c;
-			     }
-			   }
-			   cypherText = new String(k);
-			   return cypherText;
+				k[i] = c;
+			}
+		}
+		cypherText = new String(k);
+		return cypherText;
+	
 	}
 
+	
+	
 	@Override
-	public List<String> decrypt(List<String> cypherTexte)
-			throws CrypterException {
-		// TODO Auto-generated method stub
-		return null;
+	public List<String> decrypt(List<String> cypherTexte) throws CrypterException {
+		
+		List<String> ergebnis = new LinkedList<String>();
+		for (String message : cypherTexte) {
+			ergebnis.add(decrypt(message));
+		}
+		return ergebnis;
 	}
- 
+
 }
--- TPE_UIB01_uebung04/src/hs/mannheim_TPE_UIB01_Uebung04/test.java
+++ TPE_UIB01_uebung04/src/hs/mannheim_TPE_UIB01_Uebung04/test.java
@@ -5,47 +5,32 @@ import java.util.List;
 
 public class test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws CrypterException {
 
+		
 		List<String> list = new ArrayList<String>();
 		list.add("Ziel");
-		list.add("Ziel");
-
-		System.out.println(encrypt("Ziel"));
-
-	}
-
-	public static int key = 3;
-
-	public static String encrypt(String message) {
-
-		message = message.toUpperCase();
-		char[] k = message.toCharArray(); 
-
-		for (int i = 0; i < message.length(); i++) {
-			char c = message.charAt(i); 
-
-			if (((int) (c) >= 65) && ((int) (c) <= (90 - key))) { 																									
-				c = (char) (c + key);
-				k[i] = c;
-			} else if (((int) (c) > (90 - key)) && ((int) (c) <= 90)) { 														
-				c = (char) (c + key - 26);
-				k[i] = c;
-			}
-		}
-		message = new String(k);
-		return message;
-	}
-
-	public static List<String> encrypt(List<String> messages) {
-		List<String> messages2 = new ArrayList<String>();
-
-		do {
-			String a = encrypt(messages.iterator().next());
-			messages2.add(a);
-		} while (messages.iterator().hasNext());
-
-		return messages2;
-	}
-
+		list.add("Messer");
+		list.add("Caesar");
+		
+		
+		
+		/*
+		CrypterCaesar a = new CrypterCaesar("C");
+		
+		System.out.println(a.encrypt("CAESAR"));
+		System.out.println(a.encrypt(list));
+		
+		List<String> list2 = new ArrayList<String>();
+		list2.add("CLHO");
+		list2.add("PHVVHU");
+		
+		System.out.println(a.decrypt("FDHVDU"));
+		System.out.println(a.decrypt(list2));
+		
+		*/
+		CrypterSubstitution b = new CrypterSubstitution("UFLPWDRASJMCONQYBVTEXHZKGI");
+		
+		System.out.println(b.encrypt("WIKIPEDIAISTINFORMATIV"));
+	}	
 }
